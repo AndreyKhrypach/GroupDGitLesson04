4c5424534ba978004d73cee5aa69ee3a8060d086
AndreyKhrypach <pacik78@gmail.com>
2024-01-09 20:42:10 +0200

    add new system out println

--- src/GroupDLesson04GitProgram.java
+++ src/GroupDLesson04GitProgram.java
@@ -2,5 +2,6 @@ public class GroupDLesson04GitProgram {
 
     public static void main(String[] args){
         System.out.println("Hello world from Andrey to Git");
+        System.out.println("Second commit to git");
     }
 }
